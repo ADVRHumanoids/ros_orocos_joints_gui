476945ad3e3e86f76c6a75b7a42786cc4144bd1c
Alessio Rocchi <alessio.rocchi@iit.it>
2016-11-28 17:34:24 +0100

    Added more information to python node, made it to run undefinitely

--- python/pyros_topic_to_param.py
+++ python/pyros_topic_to_param.py
@@ -4,28 +4,26 @@ import rospy
 import sys
 from sensor_msgs.msg import JointState
 
-_latest_msg = None
 
 def joint_states_callback(msg):
-    rospy.loginfo("pyros_topic_to_param, received message")
-    _latest_msg = msg
+    rospy.loginfo("received JointState")
+    param_dict = dict()
+    for joint_id, joint_name in enumerate(msg.name):
+        param_dict[joint_name] = msg.position[joint_id]
+
+    rospy.set_param_raw('zeros', param_dict)
 
 if __name__ == '__main__':
     """
-    simple python node that subscribes to a joint state publisher and transforms first message to a ros parameter, then quits
+    simple python node that subscribes to a joint state publisher and transforms every JointState message to a ros zeros parameter
     """
     rospy.myargv(argv=sys.argv)
     rospy.init_node('pyros_topic_to_param')
 
+    rospy.loginfo('node is running..')
+
     rospy.Subscriber('joint_states', JointState, joint_states_callback)
 
-    while _latest_msg is None:
+    while not rospy.is_shutdown():
         rospy.spin()
-
-    param_dict = dict()
-    for joint_id, joint_name in enumerate(_latest_msg.name):
-        param_dict[joint_name] = _latest_msg.position[joint_id]
-
-    rospy.set_param_raw('zeros', param_dict)
-
-    rospy.spin()
+        rospy.sleep(1.)
